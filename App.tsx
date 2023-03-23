cd58552ec5760e13b93564f147291dd329191ec4
César Hilário <cesar.hilario.dev@gmail.com>
2023-03-23 16:06:13 -0300

    chore: project setup

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Home } from './src/screens/Home';
+
+export const App = () => {
+  return (
+    <View>
+      <Home />
+    </View>
+  );
+};
